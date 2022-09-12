6ebb6f89ec2601e53ba97b01466ed77aefea24f9
tsiupaknazar <sjprime280@gmail.com>
2022-09-12 15:55:22 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8OCq3SbDO45nDSDqWiK45vOwmaXjvxLs" || process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "cryptoapp-c5da7.firebaseapp.com",
-  projectId: "cryptoapp-c5da7",
-  storageBucket: "cryptoapp-c5da7.appspot.com",
-  messagingSenderId: "100638907013",
-  appId: "1:100638907013:web:96ba32883627b0453c27ec"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
